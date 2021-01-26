c7533125976b4ddbdd49402b9d8e9574486848dc
cmudunuru <chandrasekhar.mudurunu@t-mobile.com>
2021-01-25 22:59:02 -0500

    creating and service call integration

---
+++ src/app/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http,Response } from '@angular/http';
+import { map, tap } from 'rxjs/operators';
+
+@Injectable()
+export class ProductService {
+
+  constructor(private _http:Http) { }
+
+  private _albumUrl:string = "../assets/album.json";
+
+  getAlbum(id:number):any{
+    return this._http.get(this._albumUrl).pipe(
+      tap(data => console.log("Products:"+JSON.stringify(data))),
+      map((response:any) => response.json())
+      );
+  }
+}
