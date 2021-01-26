6988cf3b85ccd2fe466d5c1c5d8c40922774d125
cmudunuru <chandrasekhar.mudurunu@t-mobile.com>
2021-01-25 23:03:07 -0500

    handled the error messages

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http,Response } from '@angular/http';
-import { map, tap } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators/map'
 
 @Injectable()
 export class ProductService {
