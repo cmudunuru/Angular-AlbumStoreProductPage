befd952e91b2ba3a6942ca63b216974e40e60eed
cmudunuru <chandrasekhar.mudurunu@t-mobile.com>
2021-01-25 23:12:39 -0500

    handled error

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,11 +6,11 @@ import { tap } from 'rxjs/operators';
 @Injectable()
 export class ProductService {
 
-  constructor(private _http:Http) { }
-
   private _albumUrl:string = "../assets/album.json";
 
-  getAlbum(id:number):any{
+  constructor(private _http:Http) { }
+
+  getAlbum(id: number){
     return this._http.get(this._albumUrl).pipe(
       tap(data => console.log("Products:"+JSON.stringify(data))),
       map((response:any) => response.json())
