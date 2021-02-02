6192372d3ed3ca63aa017194ab4f36d22bf41bb7
cmudunuru <chandrasekhar.mudurunu@t-mobile.com>
2021-02-01 23:47:26 -0500

    Added Product List Component

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products:Product[];
+
+  constructor(private _productService:ProductService) { }
+
+  ngOnInit() {
+    this._productService.getProductsNew().subscribe({
+      next:(response) => this.products = response,
+      error:console.log("Error while invoking the getProducts API")
+    });
+  }
+
+}
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,11 +3,14 @@ import { Http,Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { tap,map } from 'rxjs/operators';
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl:string = "../assets/album.json";
+
+  private _productsUrl:string = "../assets/products.json";
 
   constructor(private _http:Http) { }
 
@@ -16,6 +19,18 @@ export class ProductService {
       tap(data => console.log("Products:"+JSON.stringify(data))),
       map(response => <Album>response.json())
       );
+  }
+
+  getProductsNew():Observable<Product>{
+    return this._http.get(this._productsUrl).pipe(
+      tap(data => console.log("Products:"+JSON.stringify(data))),
+      map(response => <Product>response.json())
+      );
+  }
+
+  getProducts():Observable<Product[]>{
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json()
+    );
   }
 
   /*getAlbum(id: number) {
